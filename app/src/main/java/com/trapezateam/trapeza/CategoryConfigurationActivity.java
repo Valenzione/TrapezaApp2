f064289972f36d3cbb2559178a3b751af8564e84
Valenzione <snyderino@gmail.com>
2016-06-24 20:28:46 +0300

    Add Category Creation

---
+++ app/src/main/java/com/trapezateam/trapeza/CategoryConfigurationActivity.java
@@ -0,0 +1,13 @@
+package com.trapezateam.trapeza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CategoryConfigurationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_configuration);
+    }
+}
